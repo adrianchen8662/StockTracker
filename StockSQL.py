b313ef7a8b3c8a2d33770776dcadfe74a1448710
adrianchen8662 <adrianchen@pop-os.localdomain>
2021-01-10 17:18:29 -0500

    test

--- StockSQL.py
+++ StockSQL.py
@@ -7,20 +7,11 @@ import addFunction
 import checkFunction
 import deleteFunction
 import updateFunction
-print("Importing mysql connector")
-import mysql.connector
 print("Importing Yahoo Finance API")
-from yahoo_finance import Share
+import yfinance as yf
+#import mysql
 
-print("Connecting to mySQL")
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="adrian",
-  password="HaoRan8662@",
-  database="mydatabase"
-)
 end = False
-mycursor = mydb.cursor()
 
 print("Started! For help, type help. To end, type end\n")
 while end == False:
@@ -28,9 +19,40 @@ while end == False:
   if(inputCommand == "help"):
     helpFunction.helpFunction()
   elif(inputCommand == "addStock"):
-    print("To be implemented")
-    #addStock()
-  elif(inputCommand == "deleteStock"):
+    print("What is the ticker?")
+    ticker = input()
+    print("How long are you expecting to hold?(Short/Long)")
+    holdLength = input()
+    print("How many shares have been bought?")
+    sharesBought = input()
+    print("What day did you buy them?(YYYY-MM-DD)")
+    dateBought = input()
+    print("What price did you buy them at?")
+    price = input()
+  
+#    mydb = mysql.connector.connect(
+#      host = "localhost",
+#      user = "adrian",
+#      password = "HaoRan8662@",
+#      database = "mydatabase"
+#    )
+
+#    mycursor = mydb.cursor()
+
+    print("Checking on yahoo finance API")
+    #yep = yf.Ticker(ticker)
+    yep = yf.Ticker("GOOG")
+    tickerinfo = yep.info # problem is here
+    StockName = tickerinfo['longName']
+
+#    print("Creating mySQL entry")
+#    sql = "INSERT INTO Stocks (StockName, Ticker, HoldLength, PriceBought, SharesBought, DateBought) VALUES (%s %s %s %f %f %f)"
+#    val = (StockName, ticker, holdLength, price, sharesBought, dateBought)
+#    StockSQL.mycursor.execute(sql,val)
+#    StockSQL.mydb.commit()
+
+    print("Stock " + StockName + " successfully added!")
+ # elif(inputCommand == "deleteStock"):
     print("To be implemented")
     #deleteStock()
   elif(inputCommand == "updateStock"):
