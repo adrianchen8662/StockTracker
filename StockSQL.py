67649f9deb562deae5262d48facc1862d65d9b3c
adrianchen8662 <adrianchen@pop-os.localdomain>
2021-01-10 22:34:38 -0500

    cleanup

--- StockSQL.py
+++ StockSQL.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
 print("StockTracker, built by Adrian")
-print("Importing functions")
-import helpFunction
-import addFunction
-import checkFunction
-import deleteFunction
-import updateFunction
-print("Importing Yahoo Finance API")
-import yfinance as yf
-#import mysql
+print("Importing yfinance")
+import yfinance
+print("Importing mysql")
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "adrian",
+  password = "HaoRan8662@",
+  database = "mydatabase"
+)
+mycursor = mydb.cursor()
+print("Connected to mySQL server")
 
 end = False
-
 print("Started! For help, type help. To end, type end\n")
+
 while end == False:
-  inputCommand = input();
+  inputCommand = input()
+
+  # help command
   if(inputCommand == "help"):
-    helpFunction.helpFunction()
+    print("List of Commands")
+    print("help - list of commands and descriptions")
+    print("addStock - adds a stock from tracking")
+    print("deleteStock - deletes a stock from tracking")
+    print("updateStock - refreshes the stock prices from the Yahoo Finance API")
+    print("checkStats - checks the various statistics of the portfolio")
+    print("end - ends the program")
+  
+  # Adds a stock to tracking in the mySQL Database. Also verifies it exists. 
   elif(inputCommand == "addStock"):
     print("What is the ticker?")
     ticker = input()
@@ -29,41 +43,40 @@ while end == False:
     dateBought = input()
     print("What price did you buy them at?")
     price = input()
+    print("Checking the stock on the yahoo finance API")
+    stockTicker = yfinance.Ticker(ticker)
+    print("Stock found! Creating mySQL entry")
+    stockInfo = stockTicker.info
+    stockName = stockInfo['longName']
+
+    sql = "INSERT INTO Stocks(SN, T, HL, PB, SB, DB) VALUES (%s %s %s %f %f %s)"
+    val = (stockName, ticker, holdLength, price, sharesBought, dateBought)
+
+    print(price + " dollars of stock from " + stockName + " successfully added!")
   
-#    mydb = mysql.connector.connect(
-#      host = "localhost",
-#      user = "adrian",
-#      password = "HaoRan8662@",
-#      database = "mydatabase"
-#    )
-
-#    mycursor = mydb.cursor()
-
-    print("Checking on yahoo finance API")
-    #yep = yf.Ticker(ticker)
-    yep = yf.Ticker("GOOG")
-    tickerinfo = yep.info # problem is here
-    StockName = tickerinfo['longName']
-
-#    print("Creating mySQL entry")
-#    sql = "INSERT INTO Stocks (StockName, Ticker, HoldLength, PriceBought, SharesBought, DateBought) VALUES (%s %s %s %f %f %f)"
-#    val = (StockName, ticker, holdLength, price, sharesBought, dateBought)
-#    StockSQL.mycursor.execute(sql,val)
-#    StockSQL.mydb.commit()
-
-    print("Stock " + StockName + " successfully added!")
- # elif(inputCommand == "deleteStock"):
+  # deletes a stock from tracking
+  elif(inputCommand == "deleteStock"):
     print("To be implemented")
-    #deleteStock()
+
+  # update stock command
   elif(inputCommand == "updateStock"):
     print("To be implemented")
-    #updateStock()
+
+  # check statistics command
   elif(inputCommand == "checkStats"):
-    print("To be implemented")
-    #checkStats()
+    print("Check all or specific?")
+    inputCommand = input()
+    if(inputCommand == "all"):
+      print("To be implemented")
+    else:
+      print("To be implemented")
+
+  # ends the program
   elif(inputCommand == "end"):
-    print("Shutting down")
+    print("Ending session")
     end = True
+
+  # catches invalid queries
   else:
-    print("Command not recognized!");
+    print("Not a command!")
   print()
